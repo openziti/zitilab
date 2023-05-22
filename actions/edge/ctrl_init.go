26d0a11ace896f28c29fa9b98939a3da5ac4e410
Paul Lorenz <paul.lorenz@netfoundry.io>
2023-05-22 09:53:23 -0400

    Add more HA functionality

--- actions/edge/ctrl_init.go
+++ actions/edge/ctrl_init.go
@@ -41,3 +41,37 @@ func (init *edgeInit) Execute(m *model.Model) error {
 type edgeInit struct {
 	componentSpec string
 }
+
+func InitRaftController(componentSpec string) model.Action {
+	return &raftInit{
+		componentSpec: componentSpec,
+	}
+}
+
+func (init *raftInit) Execute(m *model.Model) error {
+	username := m.MustStringVariable("credentials.edge.username")
+	password := m.MustStringVariable("credentials.edge.password")
+
+	if username == "" {
+		return errors.New("variable credentials/edge/username must be a string")
+	}
+
+	if password == "" {
+		return errors.New("variable credentials/edge/password must be a string")
+	}
+
+	for _, c := range m.SelectComponents(init.componentSpec) {
+		sshConfigFactory := lib.NewSshConfigFactory(c.GetHost())
+
+		tmpl := "set -o pipefail; /home/%s/fablab/bin/ziti agent controller init %s %s default.admin 2>&1 | tee logs/controller.edge.init.log"
+		if err := host.Exec(c.GetHost(), fmt.Sprintf(tmpl, sshConfigFactory.User(), username, password)).Execute(m); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type raftInit struct {
+	componentSpec string
+}
